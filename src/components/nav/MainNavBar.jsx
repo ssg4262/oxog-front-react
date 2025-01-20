bbcc6eeffaf4600096b1b2fb50a5dfa962e332aa
고승범 <rhtmdqja4262@naver..com>
2025-01-20 17:53:24 +0900

    feat : router 추가,component 분리

---
+++ src/components/nav/MainNavBar.jsx
@@ -0,0 +1,8 @@
+const MainNavBar = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default MainNavBar;
---
+++ src/pages/main/MainHome.jsx
@@ -0,0 +1,13 @@
+import MainNavBar from "../../components/nav/MainNavBar.jsx";
+
+const MainHome = () => {
+    return (
+        <>
+            {/*navbar*/}
+            <MainNavBar />
+            {/*navbar*/}
+        </>
+    )
+}
+
+export default MainHome;
